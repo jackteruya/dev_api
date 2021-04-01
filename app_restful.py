4bc72f0c6aaba2689de5a6eb56c9db77062703e4
jackteruya <jack.teruya@gmail.com>
2021-04-01 09:35:33 -0400

    Incluido requirements do projeto, o app_restful com um exemplo de aplicação com a extensão flask-restful e incluido o módulo habilidade para gerenciar as habilidades que pode ser associadas ao desenvolvedor.

---
+++ app_restful.py
@@ -0,0 +1,60 @@
+from flask import Flask, request
+from flask_restful import Resource, Api
+import json
+from habilidades import Habilidades
+
+app = Flask(__name__)
+api = Api(app)
+
+desenvolvedores = [
+    {
+         'id':'0',
+         'nome':'Jackson',
+         'habilidades':['Python','Flask']
+     },
+    {
+         'id':'1',
+         'nome':'Teruya',
+         'habilidades':['Python','Django']}
+]
+
+# devolve um desenvolvedor pelo ID, também altera e deleta um desenvolvedor
+class Desenvolvedor(Resource):
+    def get(self, id):
+        try:
+            response = desenvolvedores[id]
+        except IndexError:
+            mensagem = 'Desenvolvedor de ID {} não existe'.format(id)
+            response = {'status':'erro', 'mensagem':mensagem}
+        except Exception:
+            mensagem = 'Erro desconhecido. Procure o administrador da API'
+            response = {'satatus':'erro', 'mensagem':mensagem}
+        return response
+
+    def put(self, id):
+        dados = json.loads(request.data)
+        desenvolvedores[id] = dados
+        return dados
+
+    def delete(self, id):
+        desenvolvedores.pop(id)
+        return {'status': 'sucesso', 'mensagem': 'Regsitro excluído'}
+
+# Lista tods os desenvolvedores e permite registrar um novo desenvolvedor
+class ListaDesenvolvedores(Resource):
+    def get(self):
+        return desenvolvedores
+
+    def post(self):
+        dados = json.loads(request.data)
+        posicao = len(desenvolvedores)
+        dados['id']=posicao
+        desenvolvedores.append(dados)
+
+
+api.add_resource(Desenvolvedor, '/dev/<int:id>/')
+api.add_resource(ListaDesenvolvedores, '/dev/')
+api.add_resource(Habilidades, '/habilidades/' )
+
+if __name__ == '__main__':
+    app.run(debug=True)
