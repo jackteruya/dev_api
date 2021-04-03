c3258e240d2456c13ec1d6de5a6f80c0e41a6ab0
jackteruya <jack.teruya@gmail.com>
2021-04-03 09:41:27 -0400

    Incluido os metodos PUT, GET e DELETE no módulo habilidades e incluido no app_restful.py um validador para verificar se habilidades informadas existem na lista_habilidades.

--- app_restful.py
+++ app_restful.py
@@ -1,7 +1,7 @@
 from flask import Flask, request
 from flask_restful import Resource, Api
 import json
-from habilidades import Habilidades
+from habilidades import Habilidades, lista_habilidades, ListaHabilidade
 
 app = Flask(__name__)
 api = Api(app)
@@ -47,14 +47,21 @@ class ListaDesenvolvedores(Resource):
 
     def post(self):
         dados = json.loads(request.data)
-        posicao = len(desenvolvedores)
-        dados['id']=posicao
-        desenvolvedores.append(dados)
+        dados_validados = [validar for validar in lista_habilidades if validar in dados['habilidades']]
+        if len(dados_validados) == len(dados['habilidades']):
+            posicao = len(desenvolvedores)
+            dados['id']=posicao
+            desenvolvedores.append(dados)
+            return desenvolvedores[posicao]
+        else:
+            mensagem = 'Habilidade nao cadastrada!'
+            return {'status':'erro','mensagem':mensagem, 'habilidades':lista_habilidades}
 
 
 api.add_resource(Desenvolvedor, '/dev/<int:id>/')
 api.add_resource(ListaDesenvolvedores, '/dev/')
-api.add_resource(Habilidades, '/habilidades/' )
+api.add_resource(Habilidades, '/habilidades/')
+api.add_resource(ListaHabilidade, '/habilidades/<int:id>/')
 
 if __name__ == '__main__':
     app.run(debug=True)
--- habilidades.py
+++ habilidades.py
@@ -1,7 +1,44 @@
-from flask_restful import Resource
+from flask_restful import Resource, Api
+from flask import Flask, request
+import json
 
-lista_habilidades = ['Python', 'Java', 'Flask', 'PHP', 'Django', 'JavaScript']
+app = Flask(__name__)
+api = Api(app)
+
+lista_habilidades = ['Python', 'Java', 'Flask', 'PHP', 'Django']
 
 class Habilidades(Resource):
     def get(self):
         return lista_habilidades
+
+    def post(self):
+        dados = json.loads(request.data)
+        lista_habilidades.append(dados['habilidade'])
+        return lista_habilidades[len(lista_habilidades)-1]
+
+class ListaHabilidade(Resource):
+    def get(self, id):
+        try:
+            response = {'habilidade': lista_habilidades[id]}
+        except IndexError:
+            mensagem = 'Habilidade de ID {} não existe'.format(id)
+            response = {'status': 'erro', 'mensagem': mensagem}
+        except Exception:
+            mensagem = 'Erro desconhecido. Procure o administrador da API'
+            response = {'satatus': 'erro', 'mensagem': mensagem}
+        return response
+
+    def put(self, id):
+        dados = json.loads(request.data)
+        lista_habilidades[id] = dados['habilidade']
+        return dados
+
+    def delete(self, id):
+        habilidade_deletada = lista_habilidades.pop(id)
+        return {'status': 'sucesso', 'mensagem': 'Habilidade {} excluído'.format(habilidade_deletada)}
+
+'''api.add_resource(Habilidades, '/habilidades/')
+api.add_resource(ListaHabilidade, '/habilidades/<int:id>')'''
+
+if __name__ == '__main__':
+    app.run(debug=True)
